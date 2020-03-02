49bad3adfebdf92d4975af688ffd408ad676b5cd
shanghai <shanghai.01@bytedance.com>
2020-03-02 23:01:59 +0800

    debug

--- CTFd/models.py
+++ CTFd/models.py
@@ -1087,7 +1087,8 @@ class SkuProxyInfo(db.Model):
         real_price = self.sku_info.sku_price - self.good_base_info.coupon
         ## 不用劵销售价格
         real_price_no = self.sku_info.sku_price
-
+        if real_price:
+            return
         ##正常的利润
         self.profit.profit = self.sku_info.sku_price * (1 - MALL_REBATE) - real_cost
         self.profit.profit_rate = format_float(self.profit.profit / real_price)
